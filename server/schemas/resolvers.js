bd1be675f777f16334dc94676c436cbd718b7042
Lily Thomas <lilylynettethomas@gmail.com>
2022-03-08 11:26:45 -0800

    Added resolver for user find query and login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,35 @@
 const { Book, User  } = require('../models');
-const { authMiddleware,signToken} = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-express');
+const { authMiddleware, signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        users: async () => {
+            return User.find();
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username });
+        },
 
     },
     Mutation: {
-        
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({username, email, password});
+            const token = signToken(user);
+            return {token, user};
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
     }
 };
 
