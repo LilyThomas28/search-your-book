710b7333df4373c0174c7a5078874b6206e7ed16
Lily Thomas <lilylynettethomas@gmail.com>
2022-03-08 13:20:14 -0800

    Corrected auth

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -18,9 +17,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(routes);
+// app.use(routes);
 
-// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   const server = new ApolloServer({
     typeDefs,
@@ -37,5 +35,4 @@ const startApolloServer = async (typeDefs, resolvers) => {
   })
 };
   
-// Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
