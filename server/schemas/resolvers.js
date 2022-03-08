9c1cc2def60598e464f9f095ecd7c721a58657f1
Lily Thomas <lilylynettethomas@gmail.com>
2022-03-08 11:18:48 -0800

    Added typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,14 @@
 const { Book, User  } = require('../models');
+const { authMiddleware,signToken} = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
- 
+    Query: {
+
+    },
+    Mutation: {
+        
+    }
 };
 
 module.exports = resolvers;
